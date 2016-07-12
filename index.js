a8d0812631f90efc99cf95a8fbbfa7f6cc5bddec
xiaojimao18 <xiaojimao18@gmail.com>
2016-07-12 16:27:09 +0800

    fix bugs when chunk.name is null

--- index.js
+++ index.js
@@ -8,36 +8,45 @@ function HTMLEntryPlugin(options) {
     src: '',
     ext: 'html'
   }, options);
+
+  if (!this.options.src) {
+    throwError('Please input the src path in options.');
+    return;
+  }
 }
 
 HTMLEntryPlugin.prototype.apply = function(compiler) {
   var _this = this;
 
   compiler.plugin("emit", function(compilation, callback) {
-    var dirPath = path.resolve(_this.options.src);
-    var dirName = escapePattern(path.parse(dirPath).name);
-    var extName = escapePattern(_this.options.ext);
+    try {
+      var dirPath = path.resolve(_this.options.src);
+      var dirName = escapePattern(path.parse(dirPath).name);
+      var extName = escapePattern(_this.options.ext);
 
-    var fileList = recursive(dirPath);
-    fileList.forEach(function(filePath) {
-      var pattern  = new RegExp(dirName + '[\\\\|\\\/](.+\\.' + extName + ')$');
-      var match = filePath.match(pattern);
+      var fileList = recursive(dirPath);
+      fileList.forEach(function(filePath) {
+        var pattern  = new RegExp(dirName + '[\\\\|\\\/](.+\\.' + extName + ')$');
+        var match = filePath.match(pattern);
 
-      if (match) {
-        var originContent = fs.readFileSync(filePath).toString();
-        var hashedContent = hashReplace(compilation, originContent);
+        if (match) {
+          var originContent = fs.readFileSync(filePath).toString();
+          var hashedContent = hashReplace(compilation, originContent);
 
-        compilation.assets[match[1]] = {
-          source: function() {
-            return hashedContent;
-          },
-          size: function() {
-            return hashedContent.length;
-          }
-        };
-        compilation.fileDependencies.push(filePath);
-      }
-    });
+          compilation.assets[match[1]] = {
+            source: function() {
+              return hashedContent;
+            },
+            size: function() {
+              return hashedContent.length;
+            }
+          };
+          compilation.fileDependencies.push(filePath);
+        }
+      });
+    } catch(err) {
+      console.error(err.stack);
+    }
 
     callback();
   });
@@ -53,7 +62,7 @@ function hashReplace(compilation, content) {
                     entryFile.name : 
                     entryFile.dir + '\/' + entryFile.name;
     compilation.chunks.forEach(function(chunk) {
-      if (chunk.name.replace('\\', '\/') === entryName.replace('\\', '\/')) {
+      if (chunk.name && chunk.name.replace('\\', '\/') === entryName.replace('\\', '\/')) {
         chunk.files.forEach(function(file) {
           if (path.extname(file) === entryFile.ext) {
             var distFile = compilation.outputOptions.publicPath + file;
@@ -65,7 +74,7 @@ function hashReplace(compilation, content) {
     });
 
     if (!success) {
-      throw new Error('The referenced file was not found: ' + match[1]);
+      throwError('The referenced file was not found: ' + match[1])
     }
   }
 
@@ -73,6 +82,11 @@ function hashReplace(compilation, content) {
 }
 
 function escapePattern(str) {
+  if (typeof str !== 'string') {  
+    throwError('Please input the correct string: ' + str);
+    return;
+  }
+
   var chars = ['\\', '.', '(', ')', '[', ']', '{', '}', '*', '?', '+', '^', '$'];
 
   chars.forEach(function(char) {
@@ -80,6 +94,14 @@ function escapePattern(str) {
   });
 
   return str;
+}
+
+function throwError(str) {
+  try {
+    throw new Error(str)
+  } catch(err) {
+    console.error(err.stack);
+  }
 }
 
 module.exports = HTMLEntryPlugin;
